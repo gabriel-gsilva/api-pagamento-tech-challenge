d015ad2e5d13fa83a368b1945eed300ed4d3fa5b
Helen Caroline da Silva Santos <helencaroline@MacBook-Pro-de-Helen.local>
2024-12-03 01:46:18 -0300

    update test uni | pipeline

--- test/test_lambda_criar_preferencia.py
+++ test/test_lambda_criar_preferencia.py
@@ -9,8 +9,8 @@ os.environ['MERCADOPAGO_ACCESS_TOKEN'] = 'TEST-774461802753823-103118-964b20b404
 os.environ['DYNAMODB_TABLE'] = 'MercadoPagoPreferencias'
 os.environ['API_GATEWAY_URL'] = 'http://mock-api-gateway-url'
 
-# Adiciona o diretório raiz ao PYTHONPATH
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Adiciona o diretório src ao PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 # Verifica se o módulo pode ser importado
 try:
@@ -24,7 +24,7 @@ def mock_env_vars(monkeypatch):
     monkeypatch.setenv('MERCADOPAGO_ACCESS_TOKEN', 'TEST-774461802753823-103118-964b20b4047ff5ab7b3b0c605b9b7786-176575887')
     monkeypatch.setenv('DYNAMODB_TABLE', 'MercadoPagoPreferencias')
     monkeypatch.setenv('API_GATEWAY_URL', 'http://mock-api-gateway-url')
-
+    
 @patch('lambda_criar_preferencia.sdk.preference')
 @patch('lambda_criar_preferencia.table.batch_writer')
 def test_lambda_handler_sucesso(mock_batch_writer, mock_preference, mock_env_vars):
@@ -68,7 +68,6 @@ def test_lambda_handler_sucesso(mock_batch_writer, mock_preference, mock_env_var
     assert response['statusCode'] == 200
     body = json.loads(response['body'])
     assert body['message'] == "Preferência criada e salva com sucesso"
-    assert body['id_preferencia'] is not None
     assert body['external_reference'] is not None
     assert body['itens'][0]['titulo'] == 'Produto 1'
     assert body['itens'][0]['preco'] == 100.0
