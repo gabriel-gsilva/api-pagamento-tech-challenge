1dc3e0eed408a336035857d3f802d6888df08bdf
Gabriel <gsilva.gabriel@outlook.com.br>
2024-12-03 22:29:32 -0300

    atualização variables

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "Região da AWS onde os recursos serão criados"
   type        = string
-  default     = "sa-east-1"
+  default     = "us-east-1"
 }
 
 variable "aws_account_id" {
